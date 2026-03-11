b4d7e91248a1db2b56cde4aac0d02751dc7ef131
Md kamruzzaman <kamrul.bubtr@gmail.com>
2026-03-11 17:39:46 +0600

    update server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
-import app from "./app";
+import { Server } from 'http';
+import app from './app';
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function main() {
+  try {
+    const server: Server = app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+main();
